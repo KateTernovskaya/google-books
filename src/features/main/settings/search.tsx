e57795680e84da1bf123aaef24b45c97fca1aed2
Kate <kate-ternovskaya99@mail.ru>
2024-07-09 10:36:07 +0300

    refactor adaptive

--- src/features/main/settings/search.tsx
+++ src/features/main/settings/search.tsx
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
 import { IconButton, Paper, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import s from "features/main/main.module.css";
 import { SearchProps } from "components/types";
 
 export const Search = ({ getBooks, searchChange, error, setError }: SearchProps) => {
-  const searchBook = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (error !== null) setError(null);
-    if (e.charCode === 13) getBooks();
+  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (error) setError(null);
+    if (e.key === "Enter") {
+      getBooks();
+    }
   };
 
   return (
-    <div>
-      <Paper className={s.search} component="form">
-        <TextField
-          variant="standard"
-          onChange={searchChange}
-          onKeyPress={searchBook}
-          label="Search book"
-          error={!!error}
-          helperText={error}
-          fullWidth
-        />
+    <Paper className={s.search} component="form">
+      <input type="text" className={s.none} />
+      <TextField
+        variant="standard"
+        onChange={searchChange}
+        onKeyUp={onKeyPressHandler}
+        label={error ? error : "Search book"}
+        error={!!error}
+        helperText={error}
+        fullWidth
+      />
 
-        <IconButton type="button" aria-label="search" onClick={getBooks}>
-          <SearchIcon fontSize="large" />
-        </IconButton>
-      </Paper>
-    </div>
+      <IconButton type="button" aria-label="search" onClick={getBooks}>
+        <SearchIcon fontSize="large" />
+      </IconButton>
+    </Paper>
   );
 };
